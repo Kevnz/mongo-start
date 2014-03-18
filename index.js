e9afbaa640f1dcca37ea6d7eb45a8c04d5ace736
Kevin Isom <kevin.isom@gmail.com>
2014-03-19 00:14:57 +1300

    added different env options when loading config.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var env = process.env.NODE_ENV || 'test';
+
 var qconf = require('qconf'),
-    config = qconf();
+    config = qconf({},['file://config/config.json', 'file://config/config.'+ env + '.json']);
 
 module.exports = function (collection) {
     var mongojs = require('mongojs');
