ede827c5e17450395f52bcd7d8c15fc95eac8d24
Kevin Isom <kevin.isom@gmail.com>
2015-03-01 15:40:13 +1300

    Check for connection and throw if needed

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var config = require('xtconf')();
 
 module.exports = function (collection) {
     var mongojs = require('mongojs');
-    var db = mongojs(config.get('mongo-connection')); 
+    var connectionString = config.get('mongo-connection');
+    if (connectionString === undefined) {
+        throw new Error('No connection string is defined');
+    }
+    var db = mongojs(config.get('mongo-connection'));
     return typeof collection === "string" ? db.collection(collection) : db;
 };
