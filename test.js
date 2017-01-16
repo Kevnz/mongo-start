441d25eb933045a0030f8bc60c8eba28b8414037
Kevin Isom <kevin.isom@gmail.com>
2017-01-16 17:22:53 +1300

    use furor, and clean tests

--- test.js
+++ test.js
@@ -1,70 +1,64 @@
 
-var test = require("tap").test
-var mock_xtconf = function (){
-    console.log('mock mock_xtconf')
-    return {
-        get:function () {
-            console.log('mock get')
-            return "configstring";
-        }
+const test = require('tap').test;
+
+const fuxor = require('fuxor');
+
+const mock_xtconf = function (){
+  return {
+    get:function () {
+      return 'configstring';
     }
+  }
 };
-var mock_mongojs = function (c) {
-    console.log('mock mongo');
- 
-        if (c) {
-            console.log('mock collection');
-
-            var obj = {
-                collection: function () { 
-                    return {
-                       find: function (callback) {
-                            callback(null, [0,1]);
-                       }
-                   }
-                }
-            }
-             
-            obj[c] = {
-                   find: function (callback) {
-                        callback(null, [0,1]);
-                   }
-            };
-            console.log(obj);
-
-        return obj;
- 
-    } else {
-        return {};
+const mock_mongojs = function (c) {
+  if (c) {
+    const obj = {
+      collection: function () {
+        return {
+           find: function (callback) {
+            callback(null, [0,1]);
+           }
+         }
+      }
     }
-} 
-var proxyquire =  require('proxyquire');
+    obj[c] = {
+       find: function (callback) {
+        callback(null, [0,1]);
+       }
+    };
+    return obj;
+  }
+  return {};
+}
+fuxor.add({
+  name: 'mongojs',
+  result: mock_mongojs
+});
 
+fuxor.add({ name:'xtconf', result: mock_xtconf });
 
-test("mongo-start tests", function(te) {
+test('mongo-start tests', (te) => {
 
-        module = proxyquire("./index.js", { 'mongojs': mock_mongojs, 'xtconf': mock_xtconf});
+  let module = require('./index.js');
 
-    test("Make sure mongo collection is a collection", function (t) {
-        t.plan(2);
-        console.log('Make sure mongo collection is a collection')
-        console.log(module);
-        var testDB = module('test'); 
+  test('Make sure mongo collection is a collection', (t) => {
+    t.plan(2);
+    const testDB = module('test');
 
-        testDB.find(function(err, docs) {
-            t.notOk(err, "There should be no error"); 
-            t.ok(docs.length > 0, "should return none empty items");
-            t.end();
-        });
+    testDB.find(function(err, docs) {
+      t.notOk(err, 'There should be no error');
+      t.ok(docs.length > 0, 'should return none empty items');
+      t.end();
     });
+  });
 
-    test("Should return db instance if no collection is passed" , function (t) {
-        t.plan(2);
-        var testDB = module(); 
-    	t.ok(testDB, "Should be there");
-    	t.type(testDB.collection, "function", "db should have a collection function");
-    	t.end();
-    });
+  test('Should return db instance if no collection is passed', (t) => {
+    t.plan(2);
+    const testDB = module();
+    t.ok(testDB, 'Should be there');
+    t.type(testDB.collection, 'function', 'db should have a collection function');
+    t.end();
+  });
 
-    te.end();
+  te.end();
 })
