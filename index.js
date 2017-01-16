587c34cfccb9ef63b2d7bc969ba9a933bdd3fb69
Kevin Isom <kevin.isom@gmail.com>
2017-01-16 17:23:23 +1300

    tidy and restructure code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var config = require('xtconf')();
+const config = require('xtconf')();
+const mongojs = require('mongojs');
 
 module.exports = function (collection) {
-    var mongojs = require('mongojs');
-    var connectionString = config.get('mongo-connection');
-    if (connectionString === undefined) {
-        throw new Error('No connection string is defined');
-    }
-    var db = mongojs(config.get('mongo-connection'));
-    return typeof collection === "string" ? db.collection(collection) : db;
+  const connectionString = config.get('mongo-connection');
+  if (connectionString === undefined) {
+    throw new Error('No connection string is defined');
+  }
+  const db = mongojs(config.get('mongo-connection'));
+  return typeof collection === 'string' ? db.collection(collection) : db;
 };
 
 module.exports.mongojs = require('mongojs');
